0548f3b23fb67e9e3391ebac3a6f0c8947019d1b
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2023-03-22 18:41:09 -0600

    Update

--- sodoku.py
+++ sodoku.py
@@ -1,8 +1,6 @@
 
-import numpy as np
 
-
-def main(file):
+def txtToArray(file):
     sodoku_array = [[0]*9 for i in range(9)]
     file_txt = open(file)
     row = 0
@@ -28,11 +26,72 @@ def main(file):
         else:
             column += 1
             row = 0
+    return sodoku_array
 
-    for x in sodoku_array:
-        print(x)
+
+def solve(sodoku_array):
+    find = find_empty(sodoku_array)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(sodoku_array, i, (row, col)):
+            sodoku_array[row][col] = i
+
+            if solve(sodoku_array):
+                return True
+
+            sodoku_array[row][col] = 0
+
+    return False
+
+
+def valid(sodoku_array, num, pos):
+
+    for i in range(len(sodoku_array[0])):
+        if sodoku_array[pos[0]][i] == num and sodoku_array[1] != i:
+            return False
+
+    for i in range(len(sodoku_array)):
+        if sodoku_array[i][pos[1]] == num and sodoku_array[0] != i:
+            return False
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if sodoku_array[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+def find_empty(sodoku_array):
+    for i in range(len(sodoku_array)):
+        for j in range(len(sodoku_array[0])):
+            if sodoku_array[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+
+def printBoard(sodoku_array):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            print(sodoku_array[i][j], end=" ")
+        print()
 
 
 if __name__ == "__main__":
-    file = "sodoku.txt"
-    main(file)
+    # file = "sodoku.txt"
+    # file = "sodoku_easy.txt"
+    # file = "sodoku_mid.txt"
+    # file = "sodoku_hard.txt"
+    # file = "sodoku_evil.txt"
+    # file = "sodoku_hardest.txt"
+    array = txtToArray(file)
+    solve(array)
+    printBoard(array)
